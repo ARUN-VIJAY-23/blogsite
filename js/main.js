8f1b698e36a2430f801626456538cbac44c542dc
ARUN-VIJAY-23 <102273896+ARUN-VIJAY-23@users.noreply.github.com>
2022-07-13 13:48:55 +0530

    second push

--- js/main.js
+++ js/main.js
@@ -57,38 +57,3 @@ window.onscroll = ()=>{
     }
 
 }
-
-
-var swiper = new Swiper(".slide-content", {
-    slidesPerView: 1,
-    spaceBetween: 25,
-    slidesPerGroup: 1,
-    loop: true,
-    centerSlide:'true',
-    fade:'true',
-    grabCursor:'true',
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    breakpoints: {
-        0: {
-            slidesPerView:1,
-        },
-        600: {
-            slidesPerView:2,
-        },
-        950: {
-            slidesPerView:3,
-        },
-        1100: {
-            slidesPerView:4,
-        },
-      },
-  });
